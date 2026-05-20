7cb6e92085462de2c1ff40cfbbfd3d0168c6c7fb
Mikey Ferguson <mikeyferguson49@icloud.com>
2026-05-20 15:00:11 -0500

    n queens

--- solution.go
+++ solution.go
@@ -5493,6 +5493,9 @@ The n-queens puzzle is the problem of placing n queens on an n x n chessboard su
 Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
 
 Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+Link:
+https://leetcode.com/problems/n-queens/description/
 */
 func solveNQueens(n int) [][]string {
     return [][]string{}
